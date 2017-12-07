a406d9895545c63866f84e7c4e4bc5d3e1793d1c
zhengziqiang <zhengziqiang@qiaodata.com>
2017-12-07 17:33:57 +0800

    重建目录结构

--- Core/rupee.php
+++
@@ -1,7 +0,0 @@
-<?php
-//use Core\Core;
-define('ROOT', dirname(__DIR__));
-require_once ROOT.'/Core/Core.php';
-require_once ROOT.'/Core/DbDriver/Mysql.php';
-require_once ROOT.'/Core/Model/Model.php';
-\Core\Core::run();
