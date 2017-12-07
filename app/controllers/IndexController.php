039450f4e5b582fff9389f1c128f079157bfd0e5
Touch <535217631@qq.com>
2017-12-08 01:20:25 +0800

    放弃swoole

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo "Hello Rubee";
+        exit();
+    }
+
+}
+
--- public/server.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-//定义根目录路径
-define('ROOT', dirname(__DIR__));
-require_once ROOT.'/rubee/Rubee.php';
